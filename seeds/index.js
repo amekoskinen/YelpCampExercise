8357eb56e6b256253e45daea369941d51f27a926
amekoskinen <amekoskinen@hotmail.com>
2026-01-03 17:31:12 +0200

    first commit

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const seedHelpers = require('./seedHelpers');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) { 
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            price: `${Math.floor(Math.random() * 20) + 10}`,
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${seedHelpers.descriptors[Math.floor(Math.random() * seedHelpers.descriptors.length)]} ${seedHelpers.places[Math.floor(Math.random() * seedHelpers.places.length)]}`
+        });
+        await camp.save();
+        };
+    
+    }
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
+
