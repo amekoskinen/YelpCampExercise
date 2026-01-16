e2365b63bab77c874f528373e4d26c2d9be08a50
amekoskinen <amekoskinen@hotmail.com>
2026-01-16 12:21:42 +0200

    Refactored routing, styled rating

--- middleware.js
+++ middleware.js
@@ -22,7 +22,7 @@ module.exports.storeReturnTo = (req, res, next) => {
 module.exports.isAuthor = async(req,res,next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
-  if(!campground.author.equals(req.user._id)){
+  if(req.user._id && !campground.author.equals(req.user._id)){
     req.flash('error', 'You do not have permission to do that!')
     return res.redirect(`/campgrounds/${id}`)
   }
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,25 +4,10 @@ const catchAsync = require('../utils/catchAsync')
 const Campground = require('../models/campground');
 const Review = require('../models/review')
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
+const reviews = require('../controllers/reviews')
 
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async(req,res) => {
-  const campground = await Campground.findById(req.params.id)
-  const review = new Review(req.body.review)
-  review.author = req.user._id;
-  campground.reviews.push(review)
-  await review.save()
-  await campground.save()
-  req.flash('success','Review added!')
-  res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req,res) => {
- const {id, reviewId} = req.params;
- const campground = await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId}})
- await Review.findByIdAndDelete(reviewId)
- req.flash('success','Review deleted!')
- res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
