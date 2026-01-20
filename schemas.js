7e984053294ef587463c6bc4da176535efb24f7e
amekoskinen <amekoskinen@hotmail.com>
2026-01-20 12:54:09 +0200

    Validation and security features added

--- schemas.js
+++ schemas.js
@@ -1,12 +1,35 @@
-const joi = require('joi');
+const basejoi = require('joi');
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+    'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error('string.escapeHTML', { value})
+        return clean;  
+      }
+    }
+  }
+})
+
+const joi = basejoi.extend(extension)
 
 const campgroundSchema = joi.object({
     campground: joi.object({
-      title: joi.string().required(),
+      title: joi.string().required().escapeHTML(),
       // image: joi.string().required(),
-      description: joi.string().required(),
+      description: joi.string().required().escapeHTML(),
       price: joi.number().required().min(0),
-      location: joi.string().required()
+      location: joi.string().required().escapeHTML()
     }).required(),
     deleteImages: joi.array()
   });
@@ -14,7 +37,7 @@ const campgroundSchema = joi.object({
 const reviewSchema = joi.object({
   review: joi.object({
     rating: joi.number().required().min(1).max(5),
-    body: joi.string().required()
+    body: joi.string().required().escapeHTML()
   }).required()
 })
 
